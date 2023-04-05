778446928b6c05f4fca53e6a714ae7a56d4e003b
Vic_Rain <tmddntlr@gmail.com>
2023-04-06 03:38:03 +0900

    1.0.3 commit

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const https = require("https");
-const fs = require("fs");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 const app = express();
 const mongoDB = `mongodb+srv://vicrain:${process.env.SECRET_KEY_MONGO}@cluster0.bayd0eo.mongodb.net/udp-proto`;
-const options = {
-  cert: fs.readFileSync("./configs/certificate.pem"),
-  key: fs.readFileSync("./configs/private.pem"),
-};
+
 // IMPORT FROM OTHER FILES
 const authRouter = require("./routes/auth");
 const noticeRouter = require("./routes/notice");
@@ -32,12 +27,6 @@ mongoose
     console.log(`몽고 DB 에러 발생 ${err}`);
   });
 
-const server = https.createServer(options, app);
-
-server.listen(PORT, HOST, () => {
+app.listen(PORT, HOST, () => {
   console.log("Server is running on port", PORT);
 });
-
-// app.listen(PORT, HOST, () => {
-//   console.log("Server is running on port", PORT);
-// });
